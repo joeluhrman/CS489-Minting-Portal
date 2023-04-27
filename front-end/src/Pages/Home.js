68aa4477d8a86172850effc15417b5a2dae373a9
kevinjmurray <54908085+kevinjmurray@users.noreply.github.com>
2023-04-27 15:09:03 -0400

    Display posts

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -2,14 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "../Navigation/Navbar";
 import React, { useState } from "react";
 import abi from "../utils/Social.json";
-import {ethers} from "ethers";
+import { ethers } from "ethers";
 let web3 = require('web3');
-
-
 
 function Home() {
   const contractABI = abi.abi;
   const contractAddress = "0xb8905d195398FA271436a88aaa6C52f5E1a57883";
+  const [newPosts, setNewPosts] = useState("");
 
   const retrieveRecents = async () => {
     try {
@@ -20,32 +19,21 @@ function Home() {
         const signer = provider.getSigner();
         const Contract = new ethers.Contract(contractAddress, contractABI, signer);
         
-
         let messageA = await Contract.findMessagesByAddress("0xb0af2c6a41e7ccbb78a491744cd1d87267f37148");
         let messageU = await Contract.findMessages("Kevin");
-        let newPosts = "";
-        for(let x = 0; x < messageA.length; x++)
-        {
-          newPosts += messageA[x].text + "\n";
+        let posts = "";
+        for(let x = 0; x < messageA.length; x++) {
+          posts += "user: " + messageA[x].id + "\nmessage: " + messageA[x].text + "\n";
         }
 
-        for(let y = 0; y < messageU.length; y++)
-        {
-          newPosts += messageU[y].text + "\n";
+        for(let y = 0; y < messageU.length; y++) {
+          posts += messageU[y].text + "\n";
         }
-        alert(newPosts);
+        setNewPosts(posts);
 
-        //await newContract.addUser("Kevin");
-        // let newPosts = await Contract.displayMessages();
-        // console.log(newPosts[0].text);
-        // alert(newPosts[0].text);
-
-
-        if(newPosts.length == 0){
+        if(posts.length === 0){
           alert("No messages minted");
         }
-        //console.log(posts);
-        //figure out how to recieve the post info and then display it below
       } 
       else {
         console.log("error");
@@ -56,35 +44,16 @@ function Home() {
     }
   }
 
-    // function displayPost(e) {
-    //     return (
-    //       <div className="feedPost">
-    //         <div> 
-    //           {e.text} 
-    //           <button>Like</button>
-    //         </div>
-    //       </div>
-    //     );
-    // }
-
-    return (
-      <div className="App">
-        <Navbar />
-        <header className="App-header gradient_bg">
+  return (
+    <div className="App">
+      <Navbar />
+      <header className="App-header gradient_bg">
         <p>Home</p>
         <button onClick={retrieveRecents}>Refresh</button>
+        <div className="new-posts">{newPosts}</div>
+      </header>
+    </div>
+  );
+}
 
-        {/* <div>{posts}</div> */}
-        
-        {/* {posts.map(App => { return (
-          <a>
-              <button onClick={(e)=> displayPost(e)}> {App} </button>
-          </a>
-        )})} */}
-
-        </header>
-      </div>
-    );
-  }
-  
-  export default Home;
+export default Home;
